3d008b333a36cd9fe01bf6ef0a39ac7857eafaa5
MohamedAdhil10 <adhilm9991@gmail.com>
2025-02-17 10:11:24 +0530

    Update FAANG.py

--- FAANG.py
+++ FAANG.py
@@ -2,7 +2,6 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
-import time
 from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split, RandomizedSearchCV
